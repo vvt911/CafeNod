a47cb82237754f6e5c9e6c5bc5c57d3dca75d7a2
HoangTuan2602 <2051063482@e.tlu.edu.vn>
2022-03-29 17:22:35 +0700

    fixed header, footer, button back to top

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,6 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-
-let header = document.getElementById('header')
-let headerChange = document.getElementById('header--change')
+const headerChange = document.getElementById('header--change');
+const btnScrollTop = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
@@ -9,4 +8,14 @@ window.onscroll = function () {
     } else {
         headerChange.style.display = 'none'
     }
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        btnScrollTop.style.display = "block";
+    } else {
+        btnScrollTop.style.display = "none";
+    }
 }
+
+btnScrollTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
